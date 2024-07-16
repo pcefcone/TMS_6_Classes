2c22bbc0b8ca40a38c213438f09e9fe309d2509c
Yaroslav <yaroslav.kerez@gmail.com>
2024-07-16 19:02:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Channels;
+
+internal class Program
+{
+    class Phone
+    {
+        string number;
+        string model;
+        float weight;
+
+        public Phone()
+        {
+            number = "Unknown number";
+            model = "Unknown model";
+            weight = 0.0f;
+        }
+        public Phone(string Number, string model)
+        {
+            this.number = Number;
+            this.model = model;
+            weight = 0.0f;
+        }
+
+        public Phone(string Number, string model, float weight)
+        {
+            this.number = Number;
+            this.model = model;
+            this.weight = weight;
+        }
+
+        public void RecieveCall(string name)
+        {
+            Console.WriteLine($"{name} calls");
+        }
+
+        public string GetNumber()
+        {
+            return this.number;
+        }
+    }
+    private static void Main(string[] args)
+    {
+        Phone phoneOne = new Phone();
+        Phone phoneTwo = new Phone("+48012345678", "Apple", 170.25f);
+        Phone phoneThree = new Phone("+48205305405", "Xiaomi");
+        
+        Console.WriteLine(phoneOne.GetNumber());
+        Console.WriteLine(phoneTwo.GetNumber());
+        Console.WriteLine(phoneThree.GetNumber());
+        phoneTwo.RecieveCall("User112");
+        phoneTwo.RecieveCall("User234");
+        phoneTwo.RecieveCall("User354");
+
+    }
+}
